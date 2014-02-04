0dd3e1038137ffaf445fb13cf79f2b30233624c4
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2014-02-04 09:19:15 +0100

    Initial creation of project

---
+++ KonverteraTemperaturer/KonverteraTemperaturer/Model/TemperatureConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KonverteraTemperaturer.Model
+{
+    public class TemperatureConverter
+    {
+        public int CelsiusToFahrenheit(int degreesC) { return 1; }
+        public int FahrenheitToCelsius(int degreesF) { return 1; }
+    }
+}
